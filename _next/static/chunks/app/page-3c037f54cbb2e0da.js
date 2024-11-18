1e2946d9d49e846fe9cbb4040e0ef85c30803397
HikariIT <alice.it.mail@gmail.com>
2024-11-18 14:53:29 +0100

    Deploy

---
+++ _next/static/chunks/app/page-3c037f54cbb2e0da.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{2672:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,3782,23)),Promise.resolve().then(n.t.bind(n,6634,23))},6634:()=>{},3782:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[53,441,517,358],()=>s(2672)),_N_E=e.O()}]);
